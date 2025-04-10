4c3a1b8659496a98fe7950d39e071bf57c887a39
Your Name <you@example.com>
2025-04-10 03:24:50 -0700

    lorie

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link href=<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    
+</head>
+<body>
+   <h1 id="main_title">CRUD APPLICATION IN PHP</h1> 
+   <div class="container">
+        <table class="table table-hover table-bordered table-stripped">
+        <thread>
+             <tr>
+                 <th>ID</th>
+                 <th>First Name</th>
+                 <th>Last Name</th>
+                 <th>Age</th>
+            </tr>
+       </thread>
+       <tbody>
+           <tr>
+              <td>3</td>
+              <td>Lorie</td>
+              <td>Tac-an</td>
+              <td>21</td>
+          </tr>
+          <tr>
+              <td>3</td>
+              <td>Alyssa Mae</td>
+              <td>Rodriguez</td>
+              <td>19</td> 
+        </tr>
+    </tbody>
+   </table>
+   </div>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>   
+   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.min.js" integrity="sha384-VQqxDN0EQCkWoxt/0vsQvZswzTHUVOImccYmSyhJTp7kGtPed0Qcx8rK9h9YEgx+" crossorigin="anonymous"></script>
+</body>
+</html>
